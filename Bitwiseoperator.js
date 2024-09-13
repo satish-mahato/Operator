7fb5294da9109c04a9f8aa899540d1eb935c596f
satish7654 <satishsingh202dd+@gmail.com>
2024-09-13 21:01:19 +0545

    first commit

---
+++ Bitwiseoperator.js
@@ -0,0 +1,7 @@
+const a = 5;
+const b = 3;
+console.log(a & b); //bitwise and
+console.log(a | b); //bitwise or
+console.log(a ^ b); //bitwise xor
+console.log(a << b); //left shift b
+console.log(a >> b); //Right shift b
